85e510ca9c7984ff3540ee44daeab5a21b0c1240
Antoshevskyi Iaroslav <user@Antoshevskyis-MacBook-Pro.local>
2017-09-06 15:00:02 +0300

    First commit

---
+++ tests/gits.py
@@ -0,0 +1,51 @@
+import requests
+import unittest
+import json
+
+
+DEFAULT_HEADER = 'application/json'
+
+SUCCESS = 200
+INCORRECT_HEADER = 400
+ADDED = 201
+
+GET_USER = "/users/{user}"
+GET_FOLLOWERS = "/followers"
+GET_USER_FOLLOWERS = "/users/{user}/followers"
+GET_USER_COMMITS = "/repos/{user}/{repo}/commits"
+GET_USER_COMMIT_BY_SHA2 = "/repos/{user}/{repo}/commits/{sha}"
+GET_USER_COMMIT_BY_SHA = "https://api.github.com/repos/{user}/{repo}/commits/{sha}"
+GET_USER_COMMIT_COMMENTS_URL = "https://api.github.com/repos/n0mac/trek/commits/e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314/comments"
+GITHUB_AUTHORIZATIONS_URL = "https://api.github.com/authorizations"
+GITHUB_COMMENT_ID_URL = "https://api.github.com/repos/{user}/{repo}/comments/{id}"
+
+USER = "n0mac"
+REPO = "trek"
+SHA = "522b0fa809b8af60edf44e0fb06aad12963b8fe1"
+
+
+class TestCommitsFunctionality(unittest.TestCase):
+
+    def __init__(self, *a, **kw):
+        super(TestCommitsFunctionality, self).__init__(*a, **kw)
+        self.host = 'api.github.com'
+        self.command = 'repos'
+        self.url = 'http://{}/{}'.format(self.host, self.command)
+        self.user = 'n0mac'
+        self.repo = 'trek'
+        self.sha = '522b0fa809b8af60edf44e0fb06aad12963b8fe1'
+        self.get_user_commit_by_sha = "https://api.github.com/repos/{}/{}/commits/{}".format(self.user, self.repo, self.sha)
+
+    def test_commit_author(self):
+        status_code, body = self._get_commits()
+        self.assertEqual(status_code, SUCCESS)
+        self.assertEqual(body['commit']['author']['name'], 'n0mac')
+
+    def _get_commits(self, identificator=None):
+        _url = self.get_user_commit_by_sha
+        if identificator:
+            _url = "{}/{}".format(self.url, identificator)
+        _response1 = requests.get(_url)
+        json_o = json.loads(_response1.content)
+        return _response1.status_code, json_o
+
---
+++ tests/responses.py
@@ -0,0 +1,137 @@
+class response(object):
+    one = '''[
+    {
+        "sha": "e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314",
+        "commit": {
+            "author": {
+                "name": "n0mac",
+                "email": "smiling.n0mac@gmail.com",
+                "date": "2016-10-04T14:00:35Z"
+            },
+            "committer": {
+                "name": "n0mac",
+                "email": "smiling.n0mac@gmail.com",
+                "date": "2016-10-04T14:00:35Z"
+            },
+            "message": "fresh update",
+            "tree": {
+                "sha": "a021cb06ef573035acd9a4e1e8ea7805385ca840",
+                "url": "https://api.github.com/repos/n0mac/trek/git/trees/a021cb06ef573035acd9a4e1e8ea7805385ca840"
+            },
+            "url": "https://api.github.com/repos/n0mac/trek/git/commits/e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314",
+            "comment_count": 0
+        },
+        "url": "https://api.github.com/repos/n0mac/trek/commits/e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314",
+        "html_url": "https://github.com/n0mac/trek/commit/e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314",
+        "comments_url": "https://api.github.com/repos/n0mac/trek/commits/e9bd1f80c0bb60bc4d248d36b2a1804ac07ae314/comments",
+        "author": {
+            "login": "n0mac",
+            "id": 17597280,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/17597280?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/n0mac",
+            "html_url": "https://github.com/n0mac",
+            "followers_url": "https://api.github.com/users/n0mac/followers",
+            "following_url": "https://api.github.com/users/n0mac/following{/other_user}",
+            "gists_url": "https://api.github.com/users/n0mac/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/n0mac/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/n0mac/subscriptions",
+            "organizations_url": "https://api.github.com/users/n0mac/orgs",
+            "repos_url": "https://api.github.com/users/n0mac/repos",
+            "events_url": "https://api.github.com/users/n0mac/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/n0mac/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "committer": {
+            "login": "n0mac",
+            "id": 17597280,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/17597280?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/n0mac",
+            "html_url": "https://github.com/n0mac",
+            "followers_url": "https://api.github.com/users/n0mac/followers",
+            "following_url": "https://api.github.com/users/n0mac/following{/other_user}",
+            "gists_url": "https://api.github.com/users/n0mac/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/n0mac/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/n0mac/subscriptions",
+            "organizations_url": "https://api.github.com/users/n0mac/orgs",
+            "repos_url": "https://api.github.com/users/n0mac/repos",
+            "events_url": "https://api.github.com/users/n0mac/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/n0mac/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "parents": [
+            {
+                "sha": "522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+                "url": "https://api.github.com/repos/n0mac/trek/commits/522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+                "html_url": "https://github.com/n0mac/trek/commit/522b0fa809b8af60edf44e0fb06aad12963b8fe1"
+            }
+        ]
+    },
+    {
+        "sha": "522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+        "commit": {
+            "author": {
+                "name": "n0mac",
+                "email": "smiling.n0mac@gmail.com",
+                "date": "2016-08-16T09:23:01Z"
+            },
+            "committer": {
+                "name": "n0mac",
+                "email": "smiling.n0mac@gmail.com",
+                "date": "2016-08-16T09:23:01Z"
+            },
+            "message": "commit",
+            "tree": {
+                "sha": "40d9b15310bafc0198fc47b8899d08b732336b65",
+                "url": "https://api.github.com/repos/n0mac/trek/git/trees/40d9b15310bafc0198fc47b8899d08b732336b65"
+            },
+            "url": "https://api.github.com/repos/n0mac/trek/git/commits/522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+            "comment_count": 0
+        },
+        "url": "https://api.github.com/repos/n0mac/trek/commits/522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+        "html_url": "https://github.com/n0mac/trek/commit/522b0fa809b8af60edf44e0fb06aad12963b8fe1",
+        "comments_url": "https://api.github.com/repos/n0mac/trek/commits/522b0fa809b8af60edf44e0fb06aad12963b8fe1/comments",
+        "author": {
+            "login": "n0mac",
+            "id": 17597280,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/17597280?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/n0mac",
+            "html_url": "https://github.com/n0mac",
+            "followers_url": "https://api.github.com/users/n0mac/followers",
+            "following_url": "https://api.github.com/users/n0mac/following{/other_user}",
+            "gists_url": "https://api.github.com/users/n0mac/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/n0mac/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/n0mac/subscriptions",
+            "organizations_url": "https://api.github.com/users/n0mac/orgs",
+            "repos_url": "https://api.github.com/users/n0mac/repos",
+            "events_url": "https://api.github.com/users/n0mac/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/n0mac/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "committer": {
+            "login": "n0mac",
+            "id": 17597280,
+            "avatar_url": "https://avatars2.githubusercontent.com/u/17597280?v=4",
+            "gravatar_id": "",
+            "url": "https://api.github.com/users/n0mac",
+            "html_url": "https://github.com/n0mac",
+            "followers_url": "https://api.github.com/users/n0mac/followers",
+            "following_url": "https://api.github.com/users/n0mac/following{/other_user}",
+            "gists_url": "https://api.github.com/users/n0mac/gists{/gist_id}",
+            "starred_url": "https://api.github.com/users/n0mac/starred{/owner}{/repo}",
+            "subscriptions_url": "https://api.github.com/users/n0mac/subscriptions",
+            "organizations_url": "https://api.github.com/users/n0mac/orgs",
+            "repos_url": "https://api.github.com/users/n0mac/repos",
+            "events_url": "https://api.github.com/users/n0mac/events{/privacy}",
+            "received_events_url": "https://api.github.com/users/n0mac/received_events",
+            "type": "User",
+            "site_admin": false
+        },
+        "parents": []
+    }
+]'''
