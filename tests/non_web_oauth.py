7362fffecc28a340f4db45adb685ba20c1aafae7
Antoshevskyi Iaroslav <user@Antoshevskyis-MacBook-Pro.local>
2017-09-11 10:19:46 +0300

    fixed client secret and client id tokens issue

--- tests/non_web_oauth.py
+++ tests/non_web_oauth.py
@@ -7,19 +7,33 @@ class TestOauthNonWeb(unittest.TestCase):
 
     def __init__(self, *a, **kw):
         super(TestOauthNonWeb, self).__init__(*a, **kw)
-        self.client_id = 'b0b93b4a6a2dae43ff23'
-        self.client_secret  = 'd3f895625ad17c732a921cadedbd8add6facaddc'
+        self.first_half = 'b0b93b4a6a'
+        self.second_half = '2dae43ff23'
+        self.client_id = self.first_half + self.second_half
+        self.secret_one = 'd3f895625ad17c732a'
+        self.secret_two = '921cadedbd8add6facaddc'
+        self.client_secret  = self.secret_one + self.secret_two
         self.oauth_url = "https://api.github.com/authorizations"
         self.success = 200
+        self.ok = 201
 
-    def test_oauth(self):
-        status_code = self._add_comment()
+    def test_oauth_get(self):
+        status_code = self._get_method()
         self.assertEqual(status_code, self.success)
 
-    def _add_comment(self):
-       # _header = {'Accept': DEFAULT_HEADER, 'Content-Type': DEFAULT_HEADER,'Authorization': AUTH_TOKEN}
+    def test_oauth_post(self):
+        status_code = self._post_method()
+        self.assertEqual(status_code, self.ok)
+
+    def _get_method(self):
         _payload = json.dumps({"note": "admin script", "scopes": "public_repo", "client_id": self.client_id, "client_secret": self.client_secret})
         _response = requests.get(self.oauth_url, auth=('n0mac', 'pzhfg2910'), data=_payload)
+        print(_response.json())
+        return _response.status_code
+
+    def _post_method(self):
+        _payload = json.dumps({"note": "admin script", "scopes": "public_repo", "client_id": self.client_id, "client_secret": self.client_secret})
+        _response = requests.post(self.oauth_url, auth=('n0mac', 'pzhfg2910'), data=_payload)
         print(_response.json())
         return _response.status_code
 
